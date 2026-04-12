4a38c91bfc1a0ba7e650b8209fc0b0347437c76a
glodisg <glg14@hi.is>
2026-04-12 23:48:59 +0000

    Bara smá fínpússun á upplifun notanda og tekið aðeins til

--- src/main/java/is/vinnsla/Reitur.java
+++ src/main/java/is/vinnsla/Reitur.java
@@ -25,38 +25,23 @@ public class Reitur {
 	private final static int[] PLAYER4START = {65,66,67,68};
 	
 	
-	/*
+	/**
 	 * Default constructor
 	 */
 	public Reitur() {}
-	
-    /**
-     * @param teningur //hvar í fylkinu spilarinn er
-     * @param kall //hvaða spilari
-     * @return int //Skilar númer hvað reiturinn er sem spilarinn á að fara á
-     */
-	  
-	/*
-    public static int reitur(int teningur, int kall) {
-        int[] fylki;
-        if(kall==2) {
-            fylki = new int[] {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43};
-        } else {
-            fylki = new int[] {10,11,12,13,14,15,16,17,18,19,0,1,2,3,4,5,6,7,8,9,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,0,1,2,3,4,5,6,7,8,9,44,45,46,47};
-        }
-        if(teningur>43) {
-            return fylki[42];
-        }
-        return fylki[teningur-1];
-    }
-	 */
 
+	//delete á þessar aðferðir
 	public SimpleIntegerProperty reiturPlayer1Property() {
 		return reiturPlayer1;
 	}
-
 	public SimpleIntegerProperty reiturPlayer2Property() {
-		return reiturPlayer1;
+		return reiturPlayer2;
+	}
+	public SimpleIntegerProperty reiturPlayer3Property() {
+		return reiturPlayer3;
+	}
+	public SimpleIntegerProperty reiturPlayer4Property() {
+		return reiturPlayer4;
 	}
 
 	/**
